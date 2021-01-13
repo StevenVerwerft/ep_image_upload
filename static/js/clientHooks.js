b818356f2dd85f65fe92f4621cdbabd8f8dad55b
Steven Verwerft <Steven.Verwerft@ordina.be>
2021-01-13 12:14:34 +0100

    added hooks for images

--- static/js/clientHooks.js
+++ static/js/clientHooks.js
@@ -180,15 +180,4 @@ exports.aceInitialized = (hook, context) => {
   editorInfo.ace_removeImage = _(image.removeImage).bind(context);
 };
 
-exports.collectContentImage = (name, context) => {
-  const tname = context.tname;
-  const lineAttributes = context.state.lineAttributes;
-  if (tname === 'div' || tname === 'p') {
-    delete lineAttributes.img;
-  }
-  if (tname === 'img') {
-    lineAttributes.img = context.node.outerHTML;
-  }
-};
-
 exports.aceRegisterBlockElements = () => ['img'];
---
+++ static/js/hooks.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.collectContentImage = (name, context) => {
+    const tname = context.tname;
+    const lineAttributes = context.state.lineAttributes;
+    if (tname === 'div' || tname === 'p') {
+      delete lineAttributes.img;
+    }
+    if (tname === 'img') {
+      lineAttributes.img = context.node.outerHTML;
+    }
+  };
+
+exports.ccRegisterBlockElements = () => ["img"];
